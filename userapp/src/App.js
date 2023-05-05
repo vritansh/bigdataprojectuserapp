d6d5645ce34cb5d44a5b13a760c6abe2a1ee0889
Vritansh Kamal <vritansh14@gmail.com>
2023-05-05 11:03:22 -0400

    Update App.js

--- userapp/src/App.js
+++ userapp/src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 import FileUpload from './components/FileUpload'
+import TalkingImage from './components/TalkingImage'
+import Navbar from  './components/Navbar'
+import { Redirect, Route, Switch } from "react-router-dom";
+import {  Link } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import { useState, useEffect } from "react";
+import Button from '@mui/material/Button';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
 
 function App() {
+  const [componentToShow, setComponentToShow] = useState(<FileUpload/>);
+  const handleButtonClick = (component) => {
+    setComponentToShow(component);
+  };
+  
   return (
-    <div className="App"
- 
-    > <div
+
+    <div className="App"> 
+      <AppBar position="static">
+      <Toolbar variant="dense">
+        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          {/* Add your menu icon here */}
+        </IconButton>
+        <Typography variant="h6" color="inherit" component="div" sx={{ flexGrow: 1 }}>
+          Synthesized AI
+        </Typography>
+        <Button color="inherit" onClick={() => handleButtonClick(<TalkingImage />)}>
+          Image Conversion
+        </Button>
+      </Toolbar>
+    
+    </AppBar>
+    <div
     style={{
       marginLeft:'20px',
-      marginTop:'200px',
-
+      marginTop:'200px'
     }}
     >
-
-        <FileUpload></FileUpload>
-
+        {componentToShow}
+      
         </div>
-  
     </div>
   );
 }
